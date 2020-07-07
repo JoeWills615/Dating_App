1321e04815e716a1a844fb8c6e3763d7e41dd206
Chris Ferguson <cfergus7@gmail.com>
2020-07-07 15:02:20 -0500

    its begining to look a lot like a long night of coding

--- models/dates.js
+++ models/dates.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const {  Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Dates: extends Model {
     /**
@@ -9,8 +7,13 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    Dates.associate = function(models) {
       // define association here
+      Dates.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: true,
+        }
+      });
     }
   };
   Dates .init({
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,7 +4,7 @@ const path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
@@ -27,3 +27,75 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 };
+
+//  // user member
+// app.get("/members", isAuthenticated, function(req, res) {
+//   // find user that by id that matches req.user.id
+//   // include the Dates table that matches the user(id).
+//   db.User.findOne({
+//     where: {
+//       id: req.user.id
+//     },
+//     include: [db.Dates]
+//   }).then(users => {
+//     let usersDV = users.dataValues;
+//     // stringify the return object so we can access array values
+//     let usersJSON = JSON.stringify(users, null, 2);
+//     // create a json object
+//     let data = JSON.parse(usersJSON);
+//     // create an array from the Dates array inside data
+//     let Dates = data.Dates.map(o => o);
+//     // create an array that contains an object that has key value pairs matching the users info.
+//     let userData = [{
+//       id: usersDV.id,
+//       email:usersDV.email,
+//       password:usersDV.password
+//     }];
+//     // render the user template that has 2 partials one for rendering the users info one for handling the users Dates.
+//     return res.render('profile', {Dates: Dates, User: userData});
+//   }).catch(err => res.status(401).json(err));
+// });
+
+// app.get('/user/:id', function(req, res) {
+//   // find user that by id that matches req.params.id
+//   // include the Dates table that matches the user(id).
+//   db.User.findOne({
+//     where: {
+//       id: req.params.id
+//     },
+//       include: [db.Dates]
+//   }).then(users => {
+//     let usersDV = users.dataValues;
+//     // stringify the return object so we can access array values
+//     let usersJSON = JSON.stringify(users, null, 2);
+//     // create a json object
+//     let data = JSON.parse(usersJSON);
+//     // create an array from the Dates array inside data
+//     let Dates = data.Dates.map(o => o);
+//     // create an array that contains an object that has key value pairs matching the users info.
+//     let userData = [{
+//       id: usersDV.id,
+//       email:usersDV.email,
+//       password:usersDV.password
+//     }];
+//     // render the user template that has 2 partials one for rendering the users info one for handling the users Dates.
+//     return res.render('user', {Dates: Dates, User: userData});
+//   }).catch(err => res.status(401).json(err));
+// });
+
+//date ROUTES
+// =========================================================================
+
+// search dates
+app.get("/api/dates", (req, res) => {
+  db.Drink.findAll({
+    where: {
+      type: "dates"
+    },
+    include: [db.name, db.where, db.typeOfDate]
+  }).then(results => {
+    res.json(results);
+  });
+});
+
+module.exports = app;
