2437bf9ed365f69bba32cecb95dc9af342cf0475
Joe King <jking0186@gmail.com>
2020-07-07 15:07:18 -0500

    handlebars added and running

--- models/dates.js
+++ models/dates.js
@@ -1,25 +1,26 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Dates: extends Model {
+  class Dates extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate() {
       // define association here
     }
-  };
-  Dates .init({
-    name: DataTypes.STRING,
-    location: DataTypes.STRING,
-    typeOfDate: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Dates',
-  });
-  return Dates ;
+  }
+  Dates.init(
+    {
+      name: DataTypes.STRING,
+      location: DataTypes.STRING,
+      typeOfDate: DataTypes.STRING
+    },
+    {
+      sequelize,
+      modelName: "Dates"
+    }
+  );
+  return Dates;
 };
